4fee549ca95179755caa45fee15098373e9ba135
Firuz Afzalov <afzalov98@gmail.com>
2020-01-26 01:30:57 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,49 @@
 def welcome
-  # code #welcome here
+ puts "Welcome to the Blackjack Table"
+  
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(cart_total)
+  puts "Your cards add up to #{cart_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+    return gets.chomp 
 end
 
-def end_game
-  # code #end_game here
+def end_game(cart_total)
+  puts "Sorry, you hit #{cart_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  sum = deal_card + deal_card
+  display_card_total(sum)
+  sum
 end
 
-def hit?
-  # code hit? here
+def hit?(current_cart_total)
+  prompt_user
+  input = get_user_input
+  if input == "s"
+    current_cart_total
+  elsif input == "h"
+    deal_card + current_cart_total
+  else 
+    invalid_command
+    hit?(current_cart_total)
+ end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +51,12 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  cart_total = initial_round
+  until cart_total > 21
+  cart_total = hit?(cart_total)
+  display_card_total(cart_total)
+  end_game(cart_total)
+  end
 end
     
